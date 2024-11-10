83d9abe2097dabecab6ed7790b4f4a6a1ff47224
Carlos De Santiago <carlosddes@gmail.com>
2024-11-10 01:28:42 -0500

    Fixed vim global variable error

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,6 +19,13 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = {'vim'},
+                        },
+                    },
+                },
                 capabilities = capabilities,
             })
             lspconfig.jdtls.setup({
